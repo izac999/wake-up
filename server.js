c93681e68452e7b5c059edcf2eef6750d2669ca8
izac999 <66282558+izac999@users.noreply.github.com>
2020-08-06 23:02:14 -0600

    Update server.js
    Were the project starts. And some functions when adding URLs to the system through the nodejs express website!

--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-// server.js
-// where your node app starts
-
-// init project
 const express = require('express');
 const app = express();
 const fs = require("fs");
@@ -24,20 +20,20 @@ app.get('/admin', function(request, response) {
 app.get('/addurl', function(request, response) {
    var url = request.query["url"];
   
-  if (url != undefined)
+  if (url !== undefined)
   {
    addUrl(url).then(() => {
-     response.send('Your URL has been added! If you would like to go home please click <a href="https://awake-glitch.glitch.me/">here</a>');
+     response.send('Your URL has been added! If you would like to go home please click <a href="https://wake-up.freddyxd.repl.co">here</a>');
    }).catch((reason) => {
      if (reason == "URL_IN_DB")
      {
-       response.send('Your URL is already in our database! If you would like to go home please click <a href="https://awake-glitch.glitch.me/">here</a>');
+       response.send('Your URL is already in our database! If you would like to go home please click <a href="https://wake-up.freddyxd.repl.co">here</a>');
      } else {
-      response.send('Failed to add your URL! Please <a href="https://awake-glitch.glitch.me/">try again</a>.'); 
+      response.send('Failed to add your URL! Please <a href="https://wake-up.freddyxd.repl.co/">try again</a>.'); 
      }
    });
   } else {
-    response.send('Failed to add your URL! Please <a href="https://awake-glitch.glitch.me/">try again</a>.');
+    response.send('Failed to add your URL! Please <a href="https://wake-up.freddyxd.repl.co">try again</a>.');
   }
 });
 
@@ -51,6 +47,7 @@ function addUrl(url)
       } else {
          j.push(url); 
       }
+      //db.add(`urls`, 1);
       fs.writeFile("./urls-save.json", JSON.stringify(j), 'utf8', () => {
         res();
       });
