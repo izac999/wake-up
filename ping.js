e92010adc58dca89d44de1f1e6b7cebb65742834
izac999 <66282558+izac999@users.noreply.github.com>
2020-08-06 23:10:20 -0600

    Update ping.js
    ping.js is responsable of pinging all the URLs on the urls-save.json file.

--- ping.js
+++ ping.js
@@ -1,46 +1,47 @@
-const fs = require("fs");
-const http = require("http");
-const request = require("request");
+const fs = require('fs');
+const http = require('http');
+const request = require('request');
 
-function pingURL(url)
-{
- return new Promise((resp, rej) => {
-   request(url,{ headers: { 'User-Agent': 'Awake-Glitch' } }, (err, res, body) => {
-     if (err) {rej()} else {resp();}
-    
-     //res();
-   });
- });
+function pingURL(url) {
+	return new Promise((resp, rej) => {
+		request(url, { headers: { 'User-Agent': 'Wake-Up' } }, (err, res, body) => {
+			if (err) {
+				rej();
+			} else {
+				resp();
+			}
+
+			//res();
+		});
+	});
 }
 
-function ping()
-{
-  if (process.env.DISABLED == "YES") return;
-  
-  console.log("---");
-  
-  fs.readFile("./urls-save.json", "utf8", function(err, contents) {
-    
-      try {
-        let j = JSON.parse(contents);
-        
-        j.forEach((url) => {
-          pingURL(url).then(() => {
-            console.log(`Sucessfully pinged ${url}!`);
-          }).catch((status) => {
-            console.log(`Unable to ping ${url}! Status code: NuLl.`);
-          });
-        });
-      } catch(e) {
-        console.log(err);
-      }
-  
-    
-  });
+function ping() {
+	if (process.env.DISABLED == 'YES') return;
+
+	console.log('---');
+
+	fs.readFile('./urls-save.json', 'utf8', function(err, contents) {
+		try {
+			let j = JSON.parse(contents);
+
+			j.forEach(url => {
+				pingURL(url)
+					.then(() => {
+						console.log(`Sucessfully pinged ${url}!`);
+					})
+					.catch(status => {
+						console.log(`Unable to ping ${url}! Status code: NuLl.`);
+					});
+			});
+		} catch (e) {
+			console.log(err);
+		}
+	});
 }
 
 setInterval(() => {
- ping();
-}, 300000);  // Here is when the project pings all the URL's on the urls-save.json file every 5 mins.
+	ping();
+}, 300000); // Here is when all the URLs on the urls-save.json file are pinged every 5 minutes! You can change " 300000" to the time in ms. of how Manu times do you want the system to ping the URLs 
 
 ping();
